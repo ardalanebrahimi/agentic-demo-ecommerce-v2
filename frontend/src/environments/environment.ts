72fe552198e798efb845d6e426861745938add01
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2025-12-31 17:59:21 +0100

    change the backend url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7249',
-  apiUrlx:
+  apiUrlx: 'https://localhost:7249',
+  apiUrl:
     'https://agentic-demo-ecommerce-api.yellowpebble-3d028e02.westeurope.azurecontainerapps.io',
 };
